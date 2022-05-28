474818b1abcb498cd523bb160bd567b31023065f
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-05-28 21:01:16 +0400

    get breeds

--- src/components/Breeds/Breeds.js
+++ src/components/Breeds/Breeds.js
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getDogsAsync } from "../../redux/dogeSlice";
+
 export const Breeds = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      getDogsAsync()
+    )
+  }, [dispatch]); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <section className="breeds"></section>
   );
--- src/redux/dogeSlice.js
+++ src/redux/dogeSlice.js
@@ -1,13 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getDogsAsync = createAsyncThunk(
+  'dogeApp/getDogsAsync',
+  async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/list/all')
+    if (response.ok) {
+      const allBreeds = await response.json();
+      return { allBreeds }
+    }
+  }
+);
 
 export const dogeSlice = createSlice({
   name: 'dogeApp',
-  initialState: [],
+  initialState: {},
   reducers: {
-    getDogs: (state, action) => {
-      console.log(666)
-    },
+    allBreeds: []
   },
+  extraReducers: {
+    [getDogsAsync.fulfilled]: (state, action) => {
+      state.allBreeds = action.payload.allBreeds.message;
+    }
+  }
 });
 
 export const { getDogs } = dogeSlice.actions;
