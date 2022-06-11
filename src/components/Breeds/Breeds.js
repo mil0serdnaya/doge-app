b82dd0ec371252d0e09e0e60273b4629de02e0fc
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2022-06-11 15:39:24 +0400

    getting breedslist

--- src/components/Breeds/Breeds.js
+++ src/components/Breeds/Breeds.js
@@ -1,19 +1,22 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchBreeds } from "../../redux/dogeSlice";
+import { fetchBreedsList } from "../../redux/dogeSlice";
 
 export const Breeds = () => {
   const dispatch = useDispatch();
-  const breeds = useSelector((state) => state.dogeApp.breeds);
+  const breeds = useSelector((state) => state.dogeApp.breedsList);
 
   useEffect(() => {
     dispatch(
-      fetchBreeds()
+      fetchBreedsList()
     )
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <section className="breeds">
+      { breeds.map((breed) => (
+        <div> { breed } </div>
+      )) }
     </section>
   );
 }
--- src/redux/dogeSlice.js
+++ src/redux/dogeSlice.js
@@ -1,33 +1,26 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchBreeds = createAsyncThunk(
-  'dogeApp/fetchBreeds',
+export const fetchBreedsList = createAsyncThunk(
+  'dogeApp/fetchBreedsList',
   async () => {
     const response = await fetch('https://dog.ceo/api/breeds/list');
     const breedsJson = await response.json();
     const breedsList = breedsJson.message;
 
-    breedsList.forEach(breed => {
-      let breedData = {
-        name: breed,
-        img: null
-      }
-    });
-
-    // return breedsList;
+    return breedsList;
   }
 );
 
 export const dogeSlice = createSlice({
   name: 'dogeApp',
   initialState: {
-    breeds: [],
+    breedsList: [],
   },
   reducers: {
   },
   extraReducers: {
-    [fetchBreeds.fulfilled]: (state, action) => {
-      state.breeds.push(action.payload);
+    [fetchBreedsList.fulfilled]: (state, action) => {
+      state.breedsList.push(action.payload);
     }
   }
 });
